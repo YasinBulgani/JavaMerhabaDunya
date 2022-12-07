8f3f4ce151accdb79f16b5846cbe879aa1c3c4a2
yasin.bulgan <yasinbulgan@icloud.com>
2022-12-07 10:13:57 +0300

    İlk Ders

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,5 +6,21 @@ public class Main {
 
         System.out.println("Merhaba Dünya :D")
         ;
+        /* Yorum Satırım
+        Bu İlk Ders İçin
+
+         */
+        int x = 10;
+        int y = 20;
+        int burak;
+        burak = x+y;
+        System.out.println("Merhaba Dünya" + burak);
+        double pi = 3.14;
+        System.out.println("Merhaba Dünya" + pi);
+        String Text = "Merhaba Dünya";
+        System.out.println("Merhaba Dünya" + Text);
+
+
+
     }
 }
